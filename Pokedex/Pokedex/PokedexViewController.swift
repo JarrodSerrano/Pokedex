fe8a63b34b0dbe7e7fa8fb9db69129e6276f5496
JarrodSerrano <jserrano3159@gmail.com>
2017-04-18 10:19:40 -0600

    setup Pokedex view controller

---
+++ Pokedex/Pokedex/PokedexViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PokedexViewController.swift
+//  Pokedex
+//
+//  Created by Jarrod Serrano on 4/18/17.
+//  Copyright © 2017 Jarrod Serrano. All rights reserved.
+//
+
+import UIKit
+
+class PokedexViewController: UIViewController {
+
+
+
+    
+
+    
+    
+    
+
+}
